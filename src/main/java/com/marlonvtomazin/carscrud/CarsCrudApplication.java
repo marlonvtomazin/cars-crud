975648cd202df864fcfabe2be61f5a22e62c75cf
marlonvt <marlon_kin@hotmail.com>
2026-01-15 19:41:06 -0300

    Initial commit with Sprint project

---
+++ src/main/java/com/marlonvtomazin/carscrud/CarsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.marlonvtomazin.carscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlonvtomazin/carscrud/CarsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlonvtomazin.carscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
