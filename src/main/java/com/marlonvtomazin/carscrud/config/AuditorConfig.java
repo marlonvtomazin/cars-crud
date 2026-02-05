f540f77ceada04b06d4b15201cf9bf8568546ef4
marlonvt <marlon_kin@hotmail.com>
2026-02-04 22:21:13 -0300

    Added auditor config

---
+++ src/main/java/com/marlonvtomazin/carscrud/config/AuditorConfig.java
@@ -0,0 +1,17 @@
+package com.marlonvtomazin.carscrud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditorConfig {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of("SYSTEM");
+    }
+}
